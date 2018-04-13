2a242552e9989588e1f67d0a62666f4c075ebc40
sunnylost <sunnylost@gmail.com>
2018-04-13 16:29:54 +0800

    fix element check

--- src/lay/modules/form.js
+++ src/lay/modules/form.js
@@ -877,7 +877,7 @@ layui.define('layer', function(exports) {
             stop = null,
             DANGER = 'layui-form-danger',
             field = {},
-            elem = button.hasClass(ELEM) ? button : button.parents(ELEM).eq(0),
+            elem = button.hasClass(ELEM.substring(1)) ? button : button.parents(ELEM).eq(0),
             verifyElem = elem.find('*[lay-verify]'), //获取需要校验的元素
             formElem = button.parents('.layui-form')[0], //获取当前所在的form元素，如果存在的话
             fieldElem = elem.find('input,select,textarea'), //获取所有表单域
